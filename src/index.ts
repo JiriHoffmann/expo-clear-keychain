20365265d03d6f48ada9a0fd3d1646b7b0a46c5d
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2024-12-19 10:19:29 -0800

    fix(*): app.plugin resolution

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import {
+  type ConfigPlugin,
+  withAppDelegate,
+  createRunOncePlugin,
+} from 'expo/config-plugins';
+const pkg = require('../../package.json');
+
+const withClearKeychain: ConfigPlugin<string> = (cfg) => {
+  cfg = withAppDelegate(cfg, (config) => {
+    // TODO: create a more reliable way of inserting
+    const insertIndex = config.modResults.contents.search('launchOptions') + 15;
+    const start = config.modResults.contents.substring(0, insertIndex);
+    const insert = `
+  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+  [defaults synchronize];
+
+  BOOL shouldClearCache = [defaults boolForKey:@"ClearKeychainSwitch"];
+  if(shouldClearCache){
+    NSArray *secItemClasses = @[(__bridge id)kSecClassGenericPassword,
+                                (__bridge id)kSecClassInternetPassword,
+                                (__bridge id)kSecClassCertificate,
+                                (__bridge id)kSecClassKey,
+                                (__bridge id)kSecClassIdentity];
+    for (id secItemClass in secItemClasses) {
+        NSDictionary *spec = @{(__bridge id)kSecClass: secItemClass};
+        SecItemDelete((__bridge CFDictionaryRef)spec);
+    }
+    [defaults setBool:NO forKey:@"ClearKeychainSwitch"];
+  }
+    `;
+    const end = config.modResults.contents.substring(insertIndex + 1);
+    const result = start + insert + end;
+
+    config.modResults.contents = result;
+    return config;
+  });
+  return cfg;
+};
+
+export default createRunOncePlugin(withClearKeychain, pkg.name, pkg.version);
